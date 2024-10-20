e644cfac8fd52770db93c00b3d98fc5a1bf2f066
Yadhu <yadhuy28@gmail.com>
2024-10-20 19:00:01 +0530

    Threejs visualised

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+// tailwind css usage
+    <div className={'absolute w-full h-screen p-0 top-0 left-0'}> 
+      <div className={'h-full p-0 '}>
+        <Canvas camera = {{
+          position: [0,0,10],
+          fov:30,
+
+        }}>
+          <color attach="background" args={["#ececec"]}/>
+// three-drei usage
+          <OrbitControls/> 
+          <mesh rotation ={[Math.PI / 10,10,10]}>
+// three-fiber usage
+            <torusGeometry /> 
+            <meshNormalMaterial/>
+          </mesh>
+        </Canvas>
+      </div>
+    </div>
+  )
+}
+
+export default App
